5d78dd1e9debc9b9d78b8adb3d5fb594746ae25c
Hyre <74812707+ctesenb@users.noreply.github.com>
2022-12-15 12:59:13 -0500

    Update db.dart

--- lib/src/connection/db.dart
+++ lib/src/connection/db.dart
@@ -1,18 +1,12 @@
-/*
-host: 'mysql-43314-0.cloudclusters.net',
-      port: 19751,
-      user: 'admin',
-      password: 'adbO1NJG',
-      db: 'neo-deter'
-      */
+
 import 'package:mysql1/mysql1.dart';
 
 class Mysql {
-  static String host = 'mysql-83344-0.cloudclusters.net',
-      user = 'admin',
-      password = '01Ekrb3X',
-      db = 'control';
-  static int port = 10031;
+  static String host = '',
+      user = '',
+      password = '',
+      db = '';
+  static int port =;
 
   Mysql();
 
